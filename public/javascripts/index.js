e3b4b677d3fe7f91b9906dee37410be09bb9a924
Leonard Havlicek <lenoarad.havlicek@gmail.com>
2021-09-22 21:56:42 +0200

    Updated dependencies

---
+++ public/javascripts/index.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = "pk.eyJ1IjoibGhhdmxpY2VrIiwiYSI6ImNrdHVhbWQxOTBnaTcycHF0d2pqZDA0MXEifQ.8JvvSKz0_HbhPnlBIStE2Q"
+
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [-74.5, 40],
+	zoom: 9
+});
+
+map.on("click", (l)=>console.log(l));gg
